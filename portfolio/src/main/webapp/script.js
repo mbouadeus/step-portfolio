ba2a596e621cc82c08160cc1d10c2445515cea3a
mbouadeus <mbouadeus@gmail.com>
2020-07-07 15:12:30 +0000

    added set background feature

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -22,18 +22,45 @@ window.addEventListener('load', function () {
  * Adds a random backgournd wallpaper to the page.
  */
 function addRandomBackground() {
- const wallpapers = ['https://www.toptal.com/designers/subtlepatterns/patterns/y-so-serious-white.png',
-    'https://www.toptal.com/designers/subtlepatterns/patterns/webb.png'];
+  // Pick a random wallpaper id.
+  const id = Math.floor(Math.random() * 2);
 
-  // Pick a random wallpaper.
-  const wallpaper = wallpapers[Math.floor(Math.random() * wallpapers.length)];
-
-  // Add it to the page.
-  const bodyElement = document.getElementsByTagName('body')[0];
-  bodyElement.style.background = `url("${wallpaper}")`;
+  addBackground(id);
 }
 
+/**
+ * Adds background wallpaper that corresponds with id.
+ */
 function addBackground(id) {
     const wallpapers = ['https://www.toptal.com/designers/subtlepatterns/patterns/y-so-serious-white.png',
     'https://www.toptal.com/designers/subtlepatterns/patterns/webb.png'];
+
+    // Pick wallpaper.
+    const wallpaper = wallpapers[id];
+
+    // Add it to the page.
+    const bodyElement = document.getElementsByTagName('body')[0];
+    bodyElement.style.background = `url("${wallpaper}")`;
+}
+
+/**
+ * Returns the id of the background that has been selected.
+ */
+function getSelectedBackgroundId() {
+    // Gets index of wallpaper selected from DOM.
+    const id = Array.from(document.querySelector('.modal-body > .list-group').childNodes)
+    .findIndex(listItem => listItem.classList && listItem.classList.contains('active'));
+
+    return Math.floor(id / 2);
+}
+
+function setBackground() {
+    // Retrieves id of background option selected in menu.
+    const id = getSelectedBackgroundId();
+
+    // Sets background
+    addBackground(id);
+
+    // Closes background selection menu.
+    $('#settingsModal').modal('hide');
 }
