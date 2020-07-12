42ab7ef9e4bf46e6ac3a74c2e0311ed7b8bcbb23
mbouadeus <mbouadeus@gmail.com>
2020-07-12 19:32:23 +0000

    Add text translation using GGloud translation api.

--- portfolio/src/main/java/com/google/sps/servlets/TranslationServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/TranslationServlet.java
@@ -14,6 +14,9 @@
 
 package com.google.sps.servlets;
 
+import com.google.cloud.translate.Translate;
+import com.google.cloud.translate.TranslateOptions;
+import com.google.cloud.translate.Translation;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -22,10 +25,24 @@ import javax.servlet.http.HttpServletResponse;
 
 
 @WebServlet("/translate")
-public class PortfolioComment extends HttpServlet {
+public class TranslationServlet extends HttpServlet {
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     
+    // Get the request parameters.
+    String originalText = request.getParameter("text");
+    String languageCode = request.getParameter("languageCode");
+
+    // Do the translation.
+    Translate translate = TranslateOptions.getDefaultInstance().getService();
+    Translation translation =
+        translate.translate(originalText, Translate.TranslateOption.targetLanguage(languageCode));
+    String translatedText = translation.getTranslatedText();
+
+    // Output the translation.
+    response.setContentType("text/html; charset=UTF-8");
+    response.setCharacterEncoding("UTF-8");
+    response.getWriter().println(translatedText);
   }
 }
